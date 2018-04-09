9efece98034366e17823b9aad2b50210a2c5fb6f
yuyuan <yuyuan@2dfire.com>
2018-04-09 09:53:52 +0800

    MOD：结构调整

--- app/src/main/java/com/mmxb/utilmiao/MainActivity.java
+++ app/src/main/java/com/mmxb/utilmiao/MainActivity.java
@@ -1,13 +1,25 @@
 package com.mmxb.utilmiao;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+import com.mmxb.netmodulelib.TestMomo;
+
+import rx.subscriptions.CompositeSubscription;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private CompositeSubscription mCompositeSubscription;
+
+    TestMomo testMomo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.weather_tv);
+
     }
 }
