4453d715b7de40dfb19d8152c618579a3ca5128d
momoxiangbei <momoxiangbei@aliyun.com>
2018-06-13 11:13:48 +0800

    目录整理

---
+++ UtilMiaoApp/app/src/main/java/com/mmxb/utilmiao/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mmxb.utilmiao;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.mmxb.utilmiao.lifecycle.LifecycleObserverDemo;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
